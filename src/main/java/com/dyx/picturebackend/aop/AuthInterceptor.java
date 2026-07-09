52183d5cccbdc87efb977a3f1e275d900cc438eb
itcast <574471862@qq.com>
2026-07-09 22:16:42 +0800

    解决pom依赖

--- src/main/java/com/dyx/picturebackend/aop/AuthInterceptor.java
+++ src/main/java/com/dyx/picturebackend/aop/AuthInterceptor.java
@@ -50,7 +50,7 @@ public class AuthInterceptor {
         if (userRoleEnum == null) {
             throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
         }
-        // 要求必须有管理员权限，但用户没有管理员权限，拒绝
+        // 要求必须有管理员权限，但用户没有管理员权限
         if (UserRoleEnum.ADMIN.equals(mustRoleEnum) && !UserRoleEnum.ADMIN.equals(userRoleEnum)) {
             throw new BusinessException(ErrorCode.NO_AUTH_ERROR);
         }
